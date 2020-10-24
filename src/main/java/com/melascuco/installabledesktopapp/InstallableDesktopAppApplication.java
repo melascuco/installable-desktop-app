5fb7be6d733823a9c0fb45c0889e77a2c0d48a18
Rodriguez Perez <juan.rodriguezp@atos.net>
2020-10-24 03:43:03 +0200

    Add content from spring boot initializer

---
+++ src/main/java/com/melascuco/installabledesktopapp/InstallableDesktopAppApplication.java
@@ -0,0 +1,13 @@
+package com.melascuco.installabledesktopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstallableDesktopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstallableDesktopAppApplication.class, args);
+	}
+
+}
